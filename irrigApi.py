34ef258017c73ba10a7255bff3479b4d6d417a00
jaoroca <jaoroca@gmail.com>
2017-01-21 10:01:18 -0600

    Create irrigApi.py

---
+++ irrigApi.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python 
+from time import sleep 
+import RPi.GPIO as GPIO
+import logging
+logging.basicConfig()
+# Disables warnings and sets up type of GPIO numbering:
+
+GPIO.setmode(GPIO.BCM)
+
+
+# Set up GPIO pins:  
+sensor=[27]
+sensorPow = 18
+pump=[17]
+for e in range(0, len(sensor)):
+	GPIO.setup(pump[e], GPIO.OUT)
+	GPIO.setup(sensor[e], GPIO.IN)
+GPIO.setup(sensorPow, GPIO.OUT)
+#power off sensor vcc to preserve the sensor electrodes:
+GPIO.output(sensorPow, 0)
+
+
+
+GPIO.output(sensorPow, 1) #power to the sensor
+#sleep(0)
+print("Irrigation start")
+
+for e in range(0, len(sensor)):
+        while  GPIO.input(sensor[e]):  #reads sensor
+                GPIO.output(pump[e], 0) #water pump reley start
+                GPIO.output(pump[e], 1) #water pump reley stop
+#                GPIO.output(sensorPow, 0) #sensor power down
+print("Irrigation stop")
+
+#sched = BlockingScheduler()
+
+#Schedules job_function to be run on every day 7  and 22 o'clock
+#sched.add_job(irrigation, 'cron', hour='7,21')
+
+#sched.start()
